274f6957a723298306f1eb28ee5cfb56c3eda4cc
George Skoumas <george.skoumas@tiledb.com>
2020-12-03 13:03:50 +0200

    process from local dir

--- machine_learning/ship_detection/ingest_images_into_tiledb.py
+++ machine_learning/ship_detection/ingest_images_into_tiledb.py
@@ -2,37 +2,13 @@ import boto3
 import pandas as pd
 import numpy as np
 import tiledb
-from PIL import Image
+import cv2
 
 s3 = boto3.client('s3')
 
 bucket = 'tiledb-gskoumas'
 prefix = 'airbus_ship_detection/train_v2'
-
-
-def read_image_from_s3(bucket, key):
-    """Load image file from s3.
-
-    Parameters
-    ----------
-    bucket: string
-        Bucket name
-    key : string
-        Path in s3
-
-    Returns
-    -------
-    np array
-        Image array
-    """
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket)
-    object = bucket.Object(key)
-    response = object.get()
-    file_stream = response['Body']
-    im = Image.open(file_stream)
-    return np.array(im)
-
+BATCH_SIZE = 1024
 
 def chunks(lst, n=1024):
     """Yield successive n-sized chunks from lst."""
@@ -49,7 +25,7 @@ print('Creating TileDB array for training segments...')
 ctx = tiledb.Ctx()
 
 dom = tiledb.Domain(
-        tiledb.Dim(name="image_id", domain=(0, len(image_ids) - 1), tile=1024, dtype=np.int32),
+        tiledb.Dim(name="image_id", domain=(0, len(image_ids) - 1), tile=BATCH_SIZE, dtype=np.int32),
         tiledb.Dim(name="x_axis", domain=(0, 768 - 1), tile=768, dtype=np.int32),
         tiledb.Dim(name="y_axis", domain=(0, 768 - 1), tile=768, dtype=np.int32),
         ctx=ctx,
@@ -67,7 +43,7 @@ tiledb.Array.create(array, schema)
 
 image_chunks = chunks(image_ids)
 
-number_of_chunks = len(image_ids) // 1024
+number_of_chunks = len(image_ids) // 10000
 
 with tiledb.open(array, 'w') as train_images_tiledb:
     counter = 1
@@ -75,10 +51,13 @@ with tiledb.open(array, 'w') as train_images_tiledb:
         print('Working on chunk ' + str(counter) + 'of' + str(number_of_chunks))
         image_chunk = []
         for image_id in chunk:
-            key = prefix + '/' + image_id
-            image_chunk.append(read_image_from_s3(bucket=bucket, key=key))
+            image_path = 'train_v2/' + image_id
+            image = cv2.imread(image_path)
+
+            image_chunk.append(image.astype(np.float32))
 
         print('Inserting chunk ' + str(counter) + 'of' + str(number_of_chunks))
         train_images_tiledb[tpl[0]:tpl[1]] = np.stack(image_chunk, axis=0).view([("", np.float32), ("", np.float32), ("", np.float32)])
+        del image_chunk
 
 print('done')
